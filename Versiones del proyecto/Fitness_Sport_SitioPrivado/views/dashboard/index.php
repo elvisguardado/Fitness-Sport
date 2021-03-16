0fa3b582e59639d1c8c1e06eebb19351cd28929d
EmilioGuerra497 <80735387+EmilioGuerra497@users.noreply.github.com>
2021-03-15 22:36:35 -0600

    Add files via upload
    Creación del sitio privado

---
+++ Versiones del proyecto/Fitness_Sport_SitioPrivado/views/dashboard/index.php
@@ -0,0 +1,42 @@
+<?php
+//Se incluye la clase con las plantillas del documento
+include('../../app/helpers/dashboard_page.php');
+//Se imprime la plantilla del encabezado y se envía el titulo para la página web
+Dashboard_Page::headerTemplate('Clientes');
+?>
+
+<!--Contenedor para mostrar una tabla con datos-->
+<div class="container">
+    <h4>Productos</h4>
+    <table class="highlight responsive-table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Item Name</th>
+                <th>Item Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Alvin</td>
+                <td>Eclair</td>
+                <td>$0.87</td>
+            </tr>
+            <tr>
+                <td>Alan</td>
+                <td>Jellybean</td>
+                <td>$3.76</td>
+            </tr>
+            <tr>
+                <td>Jonathan</td>
+                <td>Lollipop</td>
+                <td>$7.00</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<?php
+//Se imprime la plantilla del pie y se envía el nombre del controlador para la página web
+Dashboard_Page::footerTemplate('index.js');
+?>
